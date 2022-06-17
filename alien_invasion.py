7686fe156d4e11bbf298a0d845fde2a45ec4c3f0
liujie <luohualis@outlook.com>
2022-06-18 03:40:53 +0800

    重构主循环 将监控事件与屏幕更新设为两个函数内方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,17 +20,20 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            self.ship.blitme()
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
