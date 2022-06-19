db62dae3cca7691228b588f379118507fb40abfa
liujie <luohualis@outlook.com>
2022-06-19 10:38:14 +0800

    修正self.bullets.update的注释 删除多余的print

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,8 +20,7 @@ class AlienInvasion:
         """
         sprite内置了update方法，所以在run_game可以直接调用
         如果不使用sprite.Group()，也可以自创一个[]空列表
-        然后不能在run_game中调用update，要在_update_screen的for读取bullet时调用update
-        因为使用的是自创的列表保存bullet，所以bullet飞出屏幕后不会有后置处理会占用内存
+        然后不能在self.bullets.update，要在_update_screen的for读取bullet时调用update
         """
         self.bullets = pygame.sprite.Group()
 
@@ -78,7 +77,6 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.bullet_rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        print(len(self.bullets))
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
