3124fb1c234ac34ee604942964b0717a43fb1e1d
liujie <luohualis@outlook.com>
2022-06-19 09:25:26 +0800

    删除消失在屏幕边缘的子弹

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,7 +30,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -64,6 +64,21 @@ class AlienInvasion:
         """创建一颗子弹，并将其加入编组bullets中"""
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除子弹"""
+        # 更新子弹位置
+        self.bullets.update()
+
+        """
+        使用for循环遍历列表（或Pygame编组）时，Python要求该列表的长度在整个循环中保持不变
+        因为不能从for循环遍历的列表或编组中删除元素，所以必须遍历编组的副本
+        """
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.bullet_rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
